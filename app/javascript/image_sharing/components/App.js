eb3c552b502689cac0ef1e1167578e4f64f4ed4a
Kevin Kirchhoff <kevin.kirchhoff@appfolio.com>
2020-04-14 10:26:31 -0700

    kjk - add footer

--- app/javascript/image_sharing/components/App.js
+++ app/javascript/image_sharing/components/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
 import { inject } from 'mobx-react';
-import Header from './Header';
-// Uncomment when needed.
-// import Footer from './Footer';
+import Footer from './Footer';
 
 class App extends Component {
   /* Add Prop Types check*/
   render() {
     return (
       <div>
-        <Header title="Tell us what you think" />
-        {/* Put your components here: Flash Message, Form, Footer */}
+        <Footer />
       </div>
     );
   }
--- app/javascript/image_sharing/components/Footer.js
+++ app/javascript/image_sharing/components/Footer.js
@@ -1,7 +1,18 @@
-import { Component } from 'react';
+import React from 'react';
 
-class Footer extends Component {
-  /* Implement your Footer component here */
-}
+const Footer = () => (
+  <footer
+    style={{
+      bottom: 0,
+      fontSize: '10px',
+      paddingBottom: 4,
+      position: 'fixed',
+      textAlign: 'center',
+      width: '100%',
+    }}
+  >
+    Copyright: AppFolio Inc. Onboarding
+  </footer>
+);
 
 export default Footer;
